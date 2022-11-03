3dcfcb1da8bf6a5e7789b8a4b28e18e90dad3330
Aldana Guillermo <galdana@andreani.com>
2022-11-03 17:27:39 -0300

    Testing if exists label in searchBar Page

--- src/components/Github-search-page/github-search-page.js
+++ src/components/Github-search-page/github-search-page.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Typography from '@mui/material/Typography'
+import TextField from '@mui/material/TextField'
 
 const GithubSearchPage = () => {
     return ( 
         <React.Fragment>
             <Typography variant="h4" component={'h2'}>Github repositories list</Typography>
+            <TextField
+              id="filterBy"
+              label="Filter by"
+            />
         </React.Fragment>
      );
 }
--- src/components/Github-search-page/github-search.page.test.js
+++ src/components/Github-search-page/github-search.page.test.js
@@ -3,9 +3,14 @@ import { render, screen} from '@testing-library/react';
 import GithubSearchPage from './github-search-page';
 
 describe('When the GithubSearchPage is mounted', () => {
+    beforeEach(()=> render(<GithubSearchPage />))
+
     it('Must display the title' ,() => {
-        render(<GithubSearchPage />)
         const caseHeading = screen.getByRole('heading',{name: /Github repositories list/i})
         expect(caseHeading).toBeInTheDocument()
+    })
+    it("Must be a input text with label 'filter by' field", () => {
+        const caseLabelField = screen.getByLabelText(/filter by/i);
+        expect(caseLabelField).toBeInTheDocument()
     })
 })
