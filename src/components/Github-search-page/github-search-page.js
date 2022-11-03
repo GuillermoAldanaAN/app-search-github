871862f628b1500bf5f389918f2055a8b855d166
Aldana Guillermo <galdana@andreani.com>
2022-11-03 18:55:39 -0300

    Test if exists table with columns

--- src/components/Github-search-page/github-search-page.js
+++ src/components/Github-search-page/github-search-page.js
@@ -1,17 +1,59 @@
-import React from "react";
-import Typography from '@mui/material/Typography'
-import TextField from '@mui/material/TextField'
-
+import React, { useState } from "react";
+import { Container, Grid, Button, TextField, Typography, Box } from '@mui/material'
 const GithubSearchPage = () => {
-    return ( 
-        <React.Fragment>
+    const [isSearching, setIsSearching] = useState(false);
+    const [isSearchApplied, setIsSearchApplied] = useState(false);
+    const handleClick = async () => {
+        setIsSearching(true);
+        await Promise.resolve();
+        setIsSearchApplied(true);
+        setIsSearching(false);
+    }
+    const renderContent = () => {
+        return isSearchApplied ?
+            (
+            <table>
+                <thead>
+                    <tr>
+                        <th>Repository</th>
+                        <th>stars</th>
+                        <th>forks</th>
+                        <th>open issues</th>
+                        <th>updated at</th>
+                    </tr>
+                </thead>
+            </table>
+            ) :
+            (
+            <Box display='flex' alignItems={'center'} justifyContent='center' height={'400px'}>
+                <Typography variant="h6" color="initial">
+                    Please provide a search option and click in the search button
+                </Typography>
+            </Box>
+            )
+
+    }
+    return (
+        <Container>
             <Typography variant="h4" component={'h2'}>Github repositories list</Typography>
-            <TextField
-              id="filterBy"
-              label="Filter by"
-            />
-        </React.Fragment>
-     );
+            <Grid container spacing={2} justifyContent="space-between">
+                <Grid item md={6} xs={12}>
+                    <TextField
+                        fullWidth
+                        variant="standard"
+                        id="filterBy"
+                        label="Filter by"
+                    />
+                </Grid>
+                <Grid item md={3} xs={12}>
+                    <Button variant="contained" color='primary' fullWidth disabled={isSearching} onClick={handleClick}>
+                        Search
+                    </Button>
+                </Grid>
+            </Grid>
+            {renderContent()}
+        </Container>
+    );
 }
- 
+
 export default GithubSearchPage;
--- src/components/Github-search-page/github-search.page.test.js
+++ src/components/Github-search-page/github-search.page.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { render, screen} from '@testing-library/react';
+import { fireEvent, render, screen, waitFor, within} from '@testing-library/react';
 import GithubSearchPage from './github-search-page';
 
-describe('When the GithubSearchPage is mounted', () => {
-    beforeEach(()=> render(<GithubSearchPage />))
+beforeEach(()=> render(<GithubSearchPage />))
 
+describe('When the GithubSearchPage is mounted', () => {
+  
     it('Must display the title' ,() => {
         const caseHeading = screen.getByRole('heading',{name: /Github repositories list/i})
         expect(caseHeading).toBeInTheDocument()
@@ -12,5 +13,51 @@ describe('When the GithubSearchPage is mounted', () => {
     it("Must be a input text with label 'filter by' field", () => {
         const caseLabelField = screen.getByLabelText(/filter by/i);
         expect(caseLabelField).toBeInTheDocument()
+    })
+    it("Must be a search button", () => {
+        const caseButton = screen.getByRole('button', {name: /search/i})
+        expect(caseButton).toBeInTheDocument();
+    })
+    it("Must be a initial message 'Please provide a search option and click in the search button'", () => {
+        const caseInitialMessage = screen.getByText(/Please provide a search option and click in the search button/i);
+        expect(caseInitialMessage).toBeInTheDocument();
+    })
+    
+})
+
+describe("When the developer does a search", () => {
+    const fireClickSearch = () => fireEvent.click(screen.getByRole('button', {name: /search/i}))
+    it("The search button should be disabled until the search is done",async () =>{
+        const caseButtonSearch = screen.getByRole('button', {name: /search/i})
+        expect(caseButtonSearch).not.toBeDisabled();
+
+        fireClickSearch()
+        expect(caseButtonSearch).toBeDisabled();
+        await waitFor(()=> {
+            expect(caseButtonSearch).not.toBeDisabled();
+        })
+
+    })
+    it('The data should be displayed as a sticky table',async () => {
+        fireClickSearch()
+
+        await waitFor(() => {
+            expect(screen.queryByText(/Please provide a search option and click in the search button/i)).not.toBeInTheDocument();
+        })
+        expect(screen.getByRole('table')).toBeInTheDocument()
+    })
+    it('The header table should contain: Repository, stars, forks, open issues and updated at', async () => {
+        fireClickSearch()
+        const table = await screen.findByRole('table')
+        const tableHeaders = within(table).getAllByRole('columnheader')
+        const [repository, stars, forks, openIssues, updatedAt] = tableHeaders;
+
+        expect(tableHeaders).toHaveLength(5);
+
+        expect(repository).toHaveTextContent(/repository/i)
+        expect(stars).toHaveTextContent(/stars/i)
+        expect(forks).toHaveTextContent(/forks/i)
+        expect(openIssues).toHaveTextContent(/open issues/i)
+        expect(updatedAt).toHaveTextContent(/updated at/i)
     })
 })
