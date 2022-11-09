615ff61f75e9ce57f02f61c6ef30b6c17cb0d6ef
Aldana Guillermo <galdana@andreani.com>
2022-11-09 11:43:56 -0300

    Modification of the api rest request with new query

--- src/services/repositoriesServices.js
+++ src/services/repositoriesServices.js
@@ -1,5 +1,6 @@
 const baseUrl = process.env.NODE_ENV === 'test' ? '' : process.env.REACT_APP_BASE_URL;
 
-export const getAllRepositories =  (q) => {
-    return fetch(`${baseUrl}/search/repositories?q=${q}&page=2&per_page=50`);
+export const getAllRepositories =  ({q , rowsPerPage, currentPage}) => {
+
+    return fetch(`${baseUrl}/search/repositories?q=${q}&page=${currentPage}&per_page=${rowsPerPage}`)
 }
